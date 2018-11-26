3de32d43172e6ca11dd39d50146b242bf6653255
aubayIT <45257148+aubayIT@users.noreply.github.com>
2018-11-26 14:02:48 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://supportit.herokuapp.com/execute.php';
+$BOT_TOKEN = '794548299:AAGVgvAFIh3hZQuzHGNb5KqbxlJvps7B5Po';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
